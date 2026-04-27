8f3836dbf798503626dcfcea2b3d993fd63fc657
the2fye <armanaryafar33@gmail.com>
2026-04-27 01:30:34 -0400

    Update server.py

--- assignment-5-the2fye-main/server.py
+++ assignment-5-the2fye-main/server.py
@@ -230,4 +230,7 @@ def get_notes():
 
 # ---------- RUN ----------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+       import os
+
+port = int(os.environ.get("PORT", 5001))
+app.run(host="0.0.0.0", port=port)
